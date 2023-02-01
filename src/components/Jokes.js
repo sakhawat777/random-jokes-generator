22da0030c62cc765b6de276731ff0ea2cac86535
vumibangladeshlimited <info@vumi.com.bd>
2023-02-01 13:15:06 +0600

    Random joke generator make done

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import spinner from '../assets/spinner1.jpg';
 
 const Jokes = () => {
+  const [joke, setJoke] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const url = "https://api.chucknorris.io/jokes/random";
+  const handleJokes = () => {
+    setIsLoading(true)
+    fetch(url)
+    .then((res) =>{
+      return res.json()
+    })
+    .then((data) => 
+    setJoke(data))
+    setIsLoading(false)
+  };
+  useEffect(() =>{
+    handleJokes()
+  },[])
   return (
     <section className="--flex-center --100vh --bg-primary">
       <div className="container --card --bg-light --p">
         <h2>Random Jokes Generator</h2>
         <div className="--line"></div>
-        <h4>Joke id: 001</h4>
+        {isLoading? (
+          <div className="--center-all">
+            <img src={spinner} alt="loading" width={100} />
+          </div>
+        ) : (
+          <>
+          <h4>Joke id: {joke.id}</h4>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas,
-          hic!
+          {joke.value}
         </p>
+          </>
+        )}
+        
         <br />
-        <button className="--btn --btn-primary">Generate Joke</button>
+        <button className="--btn --btn-primary" onClick={() => handleJokes()}>Generate Joke</button>
       </div>
     </section>
   );
